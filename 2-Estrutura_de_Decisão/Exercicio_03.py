1ee423cee343e4a4ca11c44623b3b69db21cb14d
Jeferson777 <g7harmonic@outlookl.com>
2021-11-26 04:10:17 -0300

    # Exercicio 03: Refatorado

--- 2-Estrutura_de_Decisão/Exercicio_03.py
+++ 2-Estrutura_de_Decisão/Exercicio_03.py
@@ -2,11 +2,11 @@
 3-Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a
 letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
 '''
-sexo = input('Informe o Sexo (M ou F): ')
+sexo = input('Informe o Sexo: ').upper()
 
-if (sexo == 'M' or sexo == 'm'):
+if (sexo == 'M'):
     print(f'{sexo}: Masculino. ')
-elif sexo == 'F' or sexo == 'f':
+elif sexo == 'F':
     print(f'{sexo}: Feminino. ')
 else:
-    print('Valor incorreto!')
+    print('Sexo inválido!')
