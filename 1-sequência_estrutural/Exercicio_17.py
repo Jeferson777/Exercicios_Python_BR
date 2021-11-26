827e86679b33f4158547b710855e962ec6bab131
Jeferson777 <g7harmonic@outlookl.com>
2021-11-26 02:37:35 -0300

    # Exercicio 17: Resolvido

--- 1-sequência_estrutural/Exercicio_17.py
+++ 1-sequência_estrutural/Exercicio_17.py
@@ -6,6 +6,36 @@ tinta a serem compradas e os respectivos preços em 3 situações:
 
 1-comprar apenas latas de 18 litros;
 2-comprar apenas galões de 3,6 litros;
-3-misturar latas e galões, de forma que o desperdício de tinta seja menor. Acrescente 10% de folga e sempre arredonde os
-valores para cima, isto é, considere latas cheias.
+3-misturar latas e galões, de forma que o area quadrada a ser pintada o desperdício de tinta seja menor. Acrescente 10%
+de folga e sempre arredonde os valores para cima, isto é, considere latas cheias.
 '''
+
+import math
+
+area_a_ser_pintada = 100 # float(input('Informe a medida de area a ser pintada: '))
+
+# Definindo a base para calculos:
+area_com_folga = area_a_ser_pintada * 1.1
+litros_por_metro = 6
+litros_a_serem_usados = area_com_folga / litros_por_metro
+
+# Calculo para latas de 18 litros:
+litros_por_lata = 18
+numero_de_latas = math.ceil(litros_a_serem_usados / litros_por_lata)
+valor_latas_tintas = numero_de_latas * 80
+print(f'Você deverá usar {numero_de_latas} latas de 18 litros, no valor de R${math.ceil(valor_latas_tintas)},00')
+
+# Calculo para latas de 3,6 litros:
+litros_por_galao = 3.6
+numero_de_galoes = math.ceil(litros_a_serem_usados / litros_por_galao)
+valor_galoes_tintas = numero_de_galoes * 25
+print(f'Você deverá usar {numero_de_galoes} latas de 3,6 litros, no valor de R${math.ceil(valor_galoes_tintas)},00')
+
+# Calculo otimizado para sobras de tinta de latas e galões:
+numero_de_latas = math.floor(litros_a_serem_usados / litros_por_lata)
+valor_latas_tintas = numero_de_latas * 80
+litros_faltantes = litros_a_serem_usados % litros_por_lata
+numero_de_galoes = math.ceil(litros_faltantes / litros_por_galao)
+valor_com_galoes = numero_de_galoes * 25
+valor_total = valor_latas_tintas + valor_com_galoes
+print(f'Você deverá usar {numero_de_latas} lata(s) de 18 litros mais {numero_de_galoes} galão(s) de 3.6 litros, no valor de R${valor_total},00')
