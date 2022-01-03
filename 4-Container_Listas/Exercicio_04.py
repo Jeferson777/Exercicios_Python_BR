7a4eea91e17c290598ac67d9976124039a242f64
Jeferson777 <g7harmonic@outlookl.com>
2022-01-03 19:05:22 -0300

    # Exercicio 04: Resolvido

---
+++ 4-Container_Listas/Exercicio_04.py
@@ -0,0 +1,23 @@
+"""
+Faça um Programa que leia um vetor de 10 caracteres, e diga quantas consoantes foram lidas.
+Imprima as consoantes.
+"""
+animal = 'passarinho'
+animal = list(animal)
+
+consoantes = 'bcdfghjklmnpqrstvwxyz'
+consoantes = list(consoantes)
+filtro_consoante = []
+
+# Tratando os dados:
+for caractere in animal:
+    if caractere in consoantes:  # Filtrando caracteres.:
+        filtro_consoante.append(caractere)  # Adicionando o filtro a uma lista.
+        cont = filtro_consoante.count(caractere)  # Contando evento de um mesmo caractere.
+        if cont > 1:  # Caso haja mais de um mesmo caractere:
+            filtro_consoante.remove(caractere)  # Eliminando caractere repetido:
+
+print(f'{len(filtro_consoante)} consoante: {filtro_consoante}')
+
+
+
