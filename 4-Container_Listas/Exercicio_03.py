c8f9e5114f11fe73acae53653c22f4ef9a2e62da
Jeferson777 <g7harmonic@outlookl.com>
2022-01-03 19:03:25 -0300

    # Exercicio 03: Resolvido

---
+++ 4-Container_Listas/Exercicio_03.py
@@ -0,0 +1,12 @@
+"""
+Faça um Programa que leia 4 notas, mostre as notas e a média na tela
+"""
+nota_1 = 5.0
+nota_2 = 7.0
+nota_3 = 9.0
+nota_4 = 4.0
+
+notas = [nota_1, nota_2, nota_3, nota_4]
+media = sum(notas) / 4
+print(f'Notas: {notas}')
+print(f'Média: {media}')
